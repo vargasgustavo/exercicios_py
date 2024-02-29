cb90fd352795100187cf139e158377ca3e0042d5
GUSTAVO NEUMANN VARGAS <52318827@maxacad.edu>
2024-02-29 19:54:10 -0300

    Atualização

--- lista02/ex06.py
+++ lista02/ex06.py
@@ -3,7 +3,9 @@
 n1 = int(input("Digite um valor inteiro: "))
 n2 = int(input("Digite um valor inteiro: "))
 
-if n1 % n2 == 0:
+if n2 == 0:
+    print(f"Não é possível realizar a divisão por {n2}")
+elif n1 % n2 == 0:
     print(f"{n1} é divisível por {n2}")
 else:
     print(f"{n1} não é divisível por {n2}")
